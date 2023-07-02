4e4bc6819f66df63f48fd0073ae3bc4ca2e27be3
swayam1507 <s2m0i0t3@gmail.com>
2023-07-02 12:54:58 +0530

    student list api changed & other frontend things

--- backend/models/standards.js
+++ backend/models/standards.js
@@ -3,7 +3,9 @@ const { Schema } = mongoose;
 
 const standardsSchema = new Schema({
     name: { type: String, unique: true, required: true, minLength: 2, maxLength: 20 }
-});
+},
+    { timestamps: true }
+);
 
 
 module.exports = mongoose.model('Standards', standardsSchema);
--- backend/models/student.js
+++ backend/models/student.js
@@ -6,9 +6,10 @@ const studentSchema = new Schema({
     fatherNo: { type: String, required: true },
     motherNo: { type: String, required: true },
     selfNo: { type: String, required: true },
-    standard: { type: Number, required: true },
-    fees: { type: Number, required: true },
-    created_at: { type: Date, required: true, default: Date.now }
-});
+    standard: { type: mongoose.Types.ObjectId, required: true, ref: 'Standards', index: true },
+    fees: { type: Number, required: true }
+},
+    {timestamps: true}
+);
 
 module.exports = mongoose.model('Student', studentSchema);
